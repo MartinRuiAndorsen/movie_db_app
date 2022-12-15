80dd75aed23cb39c6ed3e9f2cea2dc6080a3d0d9
martin <mndorsen@gmail.com>
2022-12-15 14:11:23 +0100

    Initiated .NET 7.0 API APP

---
+++ movie_backend/Datasets.cs
@@ -0,0 +1,17 @@
+﻿namespace movie_backend
+{
+    public class Datasets
+    {
+        public static void Unzip(string zipPath, string extractDirectory, string unzippedName)
+        {
+            try
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractDirectory);
+            } catch (Exception e)   // TODO: Potentially check different exceptions
+            {
+                Console.WriteLine($"Failed to unzip, error: { e.Message }");
+            }
+
+        }
+    }
+}
---
+++ movie_backend/Program.cs
@@ -0,0 +1,49 @@
+
+namespace movie_backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            string zipPath = "./movie_metadata.zip";
+            string extractDirectory = "./";
+            string unzippedName = "movie_metadata.csv";
+            Datasets.Unzip(zipPath, extractDirectory, unzippedName);
+
+
+
+
+
+
+
+
+            app.Run();
+
+        }
+    }
+}
