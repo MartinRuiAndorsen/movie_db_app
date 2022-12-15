af1129aa94bc5df33bb4e6a3ad51d058e1e48824
martin <mndorsen@gmail.com>
2022-12-15 15:39:19 +0100

    Changed zip to only zip specific file in archive

---
+++ movie_backend/Model/Datasets.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Compression;
+
+namespace movie_backend.Model
+{
+    public class Datasets
+    {
+        public static bool Unzip(string zipPath, string zipFilename, string extractDirectory)
+        {
+            using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+            {
+                try
+                {
+                    ZipArchiveEntry? entry = archive.GetEntry(zipFilename);
+                    if (entry is null)
+                    {
+                        return false;
+                    }
+                    entry.ExtractToFile(zipFilename, true); // Overwrite if already zipped
+                    return true;
+                } catch(Exception e)
+                {
+                    Console.WriteLine($"ERROR: {e.Message}");
+                    return false;
+                }
+            }
+        }
+    }
+}
